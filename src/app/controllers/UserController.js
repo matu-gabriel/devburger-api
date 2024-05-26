aba89692995c9d779582e758da8e43a246f91d40
matu-gabriel <matu.dev25@gmail.com>
2024-05-26 11:42:36 -0300

    Validando dados

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,22 @@
 import User from "../models/User";
 import { v4 } from "uuid";
+import * as Yup from "yup";
 
 class UserContoller {
   async store(req, res) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().min(6).required(),
+      admin: Yup.boolean(),
+    });
+
+    try {
+      schema.validateSync(req.body, { abortEarly: false });
+    } catch (err) {
+      return res.status(401).json({ error: err.errors });
+    }
+
     const { name, email, password_hash, admin } = req.body;
 
     const user = await User.create({
